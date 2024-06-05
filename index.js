35a8655321d5e6967e845c39d14136c12cae22f0
jhamilton123 <163472749+jhamilton123@users.noreply.github.com>
2024-06-04 22:53:15 -0500

    Add carousel
    Needs more work

---
+++ index.js
@@ -0,0 +1,20 @@
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+let carousel = document.querySelector(".slider");
+let translate = 0;
+
+left.addEventListener("click", () => {
+  if (translate < 0) {
+    //   console.log("Hello World");
+    translate = translate + 200;
+    carousel.style.transform = "translateX(" + translate + "px)";
+  }
+});
+
+right.addEventListener("click", () => {
+  //   console.log("Hello World");
+  if (translate > -400) {
+    translate = translate - 200;
+    carousel.style.transform = "translateX(" + translate + "px)";
+  }
+});
